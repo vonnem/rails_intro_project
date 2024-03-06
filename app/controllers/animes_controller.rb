b3bb05d29e46b4bd20fe638c1f728b87a1750152
vonnem <vonnemaurrym@gmail.com>
2024-03-06 05:21:45 -0600

    Added list of all animes into anime index

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,7 +1,9 @@
 class AnimesController < ApplicationController
   def index
+    @animes = Anime.includes(:studio).all.order("score DESC")
   end
 
   def show
+    @anime = Anime.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'studios/index'
-  get 'studios/show'
-  get 'animes/index'
-  get 'animes/show'
+  # get 'studios/index'
+  # get 'studios/show'
+  # get 'animes/index'
+  # get 'animes/show'
+  resources :animes, only: [:index, :show]
+  resources :studios, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
