c0a51ce37c07d0722cd4c2c7ff442e1eb3f4e768
vonnem <vonnemaurrym@gmail.com>
2024-03-07 16:07:14 -0600

    Added the anime_genre and genre tables

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -2,4 +2,7 @@ class Anime < ApplicationRecord
   belongs_to :studio
   validates :episodes, :title, :score, presence: true
   validates :score, :episodes, numericality: true
+
+  has_many :anime_genres
+  has_many :genres, through :movie_genres
 end
