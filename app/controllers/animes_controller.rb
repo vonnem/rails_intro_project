bb5827ea5eaa50afcaeb0769d5e89a477f92daa5
vonnem <vonnemaurrym@gmail.com>
2024-03-08 10:55:08 -0600

    Added a drop down list for the search

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -8,7 +8,17 @@ class AnimesController < ApplicationController
   end
 
   def search
-    wildcard_search = "%#{params[:keywords]}%"
-    @animes = Anime.where("title LIKE ?", wildcard_search)
+    if params[:keywords].present?
+      wildcard_search = "%#{params[:keywords]}%"
+      @animes = Anime.includes(:studio)
+                    .where("title LIKE ?", wildcard_search)
+                    .order("score DESC")
+    else
+      @animes = Anime.includes(:studio).all.order("score DESC")
+    end
+
+    if params[:studio_id].present?
+      @animes = @animes.where(studio_id: params[:studio_id])
+    end
   end
 end
